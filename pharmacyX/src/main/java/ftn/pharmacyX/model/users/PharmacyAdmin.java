3eb84a24f7f93676cc7f55e55ec03ca54a55f91c
Dragan Gozdenovic <gozden123@gmail.com>
2021-01-26 19:53:52 +0100

    PharmacyAdmin discriminatorValue

--- pharmacyX/src/main/java/ftn/pharmacyX/model/users/PharmacyAdmin.java
+++ pharmacyX/src/main/java/ftn/pharmacyX/model/users/PharmacyAdmin.java
@@ -1,5 +1,10 @@
 package ftn.pharmacyX.model.users;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("pharmacy_admin_user")
 public class PharmacyAdmin extends User {
 
 }
