7f94a70392dd3c2cd464255bf97ac7df715b5f9c
DrGozden <gozden123@gmail.com>
2021-02-02 21:19:33 +0100

    PriceList update

---
+++ pharmacyX/src/main/java/ftn/pharmacyX/controller/PriceListController.java
@@ -0,0 +1,28 @@
+package ftn.pharmacyX.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ftn.pharmacyX.dto.PriceListDTO;
+import ftn.pharmacyX.model.PriceList;
+import ftn.pharmacyX.service.PriceListService;
+
+@Controller
+@RequestMapping("/priceLists")
+public class PriceListController {
+	
+	@Autowired
+	private PriceListService priceListService;
+
+	@PostMapping()
+	public ResponseEntity<?> createPriceList(@RequestBody PriceListDTO dto) {
+		PriceList pricelist = priceListService.createPriceList(dto);
+		return new ResponseEntity<>(pricelist,HttpStatus.OK);
+	}
+
+}
