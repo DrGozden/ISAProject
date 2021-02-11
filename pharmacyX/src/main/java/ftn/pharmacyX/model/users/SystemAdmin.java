80f978ba2f4477071254f0fcd1f4ff70d8dc6e6b
Dragan Gozdenovic <gozden123@gmail.com>
2021-02-11 18:16:43 +0100

    fix

--- pharmacyX/src/main/java/ftn/pharmacyX/model/users/SystemAdmin.java
+++ pharmacyX/src/main/java/ftn/pharmacyX/model/users/SystemAdmin.java
@@ -1,5 +1,10 @@
 package ftn.pharmacyX.model.users;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("system_admin_user")
 public class SystemAdmin extends User {
 
 }
